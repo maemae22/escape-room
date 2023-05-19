55ca8326905ef2e8fecd28fceaa27a31bc9f49e6
maemae22 <maemae2@naver.com>
2023-05-19 11:52:21 +0900

    feat: #19 - 카페 검색 (지역, 키워드 검색 동적 쿼리) API 작성 (Service, Controller)
    - /cafe/search API 작성
    - List<String> loc와 String keyword를 @RequestParam을 통해 파라미터로 받을 수 있음
    - @RequestParam의 required 속성을 false로 설정함
    - Result 클래스로 컬렉션을 감싸서 향후 필요한 필드를 추가할 수 있게 함 (확장성 증가)

--- src/main/java/com/maemae/escaperoom/controller/CafeController.java
+++ src/main/java/com/maemae/escaperoom/controller/CafeController.java
@@ -2,13 +2,18 @@ package com.maemae.escaperoom.controller;
 
 import com.maemae.escaperoom.dto.CafeDTO;
 import com.maemae.escaperoom.service.CafeService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +22,22 @@ public class CafeController {
     private final CafeService cafeService;
 
     @GetMapping("/cafe/all")
-    public Page<CafeDTO> cafeAllList(@PageableDefault(size = 12, sort = "name") Pageable pageable) {
-        return cafeService.cafeAllList(pageable);
+    public Result cafeAllList(@PageableDefault(size = 12, sort = "location") Pageable pageable) {
+        Page<CafeDTO> cafeAllList = cafeService.cafeAllList(pageable);
+        return new Result(cafeAllList);
+    }
+
+    @GetMapping("/cafe/search")
+    public Result cafeSearchList(@RequestParam(required = false) List<String> loc,
+                                 @RequestParam(required = false) String keyword,
+                                 Pageable pageable) {
+        Page<CafeDTO> cafeSearchList = cafeService.cafeSearchList(loc, keyword, pageable);
+        return new Result(cafeSearchList);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
     }
 }

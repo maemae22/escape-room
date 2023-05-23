78c151592332c51f6524e1ed2fddb1932894aeb2
maemae22 <maemae2@naver.com>
2023-05-24 02:10:04 +0900

    feat: #21 - 이미지 이름으로 S3에서 img url을 찾아 반환하도록 변경
    - 현재 DB에는 이미지 url이 아닌 이미지 이름으로 저장되어 있음
    - Repository에서 받은 Page<ThemeListDTO>에서 .getContent() 으로 List<ThemeListDTO>을 꺼내 imageUrl을 S3에서 받아온 image url로 변경 후, 변경된 내용으로 새로운 Page 객체를 생성해 반환함

--- src/main/java/com/maemae/escaperoom/service/ThemeService.java
+++ src/main/java/com/maemae/escaperoom/service/ThemeService.java
@@ -4,16 +4,30 @@ import com.maemae.escaperoom.dto.ThemeListDTO;
 import com.maemae.escaperoom.repository.ThemeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ThemeService {
 
     private final ThemeRepository themeRepository;
+    private final S3UploadService s3UploadService;
 
     public Page<ThemeListDTO> themeAllList(Pageable pageable) {
-        return themeRepository.themeAllListPage(pageable);
+
+        Page<ThemeListDTO> page = themeRepository.themeAllListPage(pageable);
+
+        List<ThemeListDTO> content = page.getContent();
+
+        for (ThemeListDTO dto : content) {
+            String S3imgUrl = s3UploadService.getThumbnailPath("theme_img/"+dto.getImageUrl());
+            dto.changeImageUrl(S3imgUrl);
+        }
+
+        return new PageImpl<>(content, pageable, page.getTotalElements());
     }
 }

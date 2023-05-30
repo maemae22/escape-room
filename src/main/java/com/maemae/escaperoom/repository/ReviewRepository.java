864a22c1d7e6dcb0843bbac808ffda034675c730
maemae22 <maemae2@naver.com>
2023-05-30 21:31:32 +0900

    feat: #32 - 리뷰 1개를 조회하는 쿼리 메서드 `findReviewOneById` 작성
    - 파라미터로 받은 reviewId에 해당하는 리뷰를 Optional<Review>로 반환함

--- src/main/java/com/maemae/escaperoom/repository/ReviewRepository.java
+++ src/main/java/com/maemae/escaperoom/repository/ReviewRepository.java
@@ -3,5 +3,9 @@ package com.maemae.escaperoom.repository;
 import com.maemae.escaperoom.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Optional<Review> findReviewOneById(Long reviewId);
 }

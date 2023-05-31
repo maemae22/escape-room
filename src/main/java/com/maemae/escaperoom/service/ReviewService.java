5857f4a907b5bd91bdc43bd7d9c08aff50301f62
maemae22 <maemae2@naver.com>
2023-05-31 17:21:59 +0900

    feat: #29 - 테마 리뷰 전체 조회 API 개발 (Service, Controller)
    - [GET] /review-list API 개발
    - 파라미터로 받은 themeId에 작성된 리뷰 list를 반환함
    - Repository에서 받은 Page<Review>를 Service 단에서 Page<ReviewOneDTO>으로 변환하여 반환함
    - 페이징 처리 O (기본값 : 사이즈 10, id 기준 desc 정렬)

--- src/main/java/com/maemae/escaperoom/service/ReviewService.java
+++ src/main/java/com/maemae/escaperoom/service/ReviewService.java
@@ -5,6 +5,8 @@ import com.maemae.escaperoom.dto.ReviewOneDTO;
 import com.maemae.escaperoom.entity.Review;
 import com.maemae.escaperoom.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -47,5 +49,15 @@ public class ReviewService {
             return "failed";
         }
         return "success";
+    }
+
+    public Page<ReviewOneDTO> reviewListByThemeId(Long themeId, Pageable pageable) {
+        try {
+            Page<Review> reviewListByThemeId = reviewRepository.findReviewListByThemeId(themeId, pageable);
+            return reviewListByThemeId.map(ReviewOneDTO::new);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }

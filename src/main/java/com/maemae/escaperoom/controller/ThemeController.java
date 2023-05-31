37ac371aaec720bf7f05e5561a2f63a395fb4b0d
maemae22 <maemae2@naver.com>
2023-05-31 22:12:29 +0900

    feat: #20 - 카페가 보유한 테마 목록 조회 API 개발 (Service, Controller)
    - [GET] /cafe/theme-list API 개발
    - Service단에서 ThemeSimpleListDTO의 imageUrl을 S3에서 받아온 이미지 주소로 바꾸어서 반환함

--- src/main/java/com/maemae/escaperoom/controller/ThemeController.java
+++ src/main/java/com/maemae/escaperoom/controller/ThemeController.java
@@ -2,6 +2,7 @@ package com.maemae.escaperoom.controller;
 
 import com.maemae.escaperoom.dto.ThemeDetailDTO;
 import com.maemae.escaperoom.dto.ThemeListDTO;
+import com.maemae.escaperoom.dto.ThemeSimpleListDTO;
 import com.maemae.escaperoom.service.ThemeService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,7 +11,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,6 +32,12 @@ public class ThemeController {
     public Result themeDetail(@PathVariable Long themeId) {
         ThemeDetailDTO themeDetailDTO = themeService.themeDetail(themeId);
         return new Result(themeDetailDTO);
+    }
+
+    @GetMapping("/cafe/theme-list")
+    public Result themeListByCafeId(@RequestParam(value = "id") Long cafeId) {
+        List<ThemeSimpleListDTO> themeListByCafeId = themeService.themeListByCafeId(cafeId);
+        return new Result(themeListByCafeId);
     }
 
     @Data

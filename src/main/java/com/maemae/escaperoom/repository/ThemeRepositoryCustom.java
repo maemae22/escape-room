c6b3dcd24af21b7c314e2f4490461f4da18452a0
maemae22 <maemae2@naver.com>
2023-06-01 18:28:26 +0900

    feat: #42 - Where 다중 파라미터 사용으로 테마 검색 동적 쿼리 작성 (Repository)
    - 페이징 처리 O : content 쿼리와 count 쿼리를 분리하여 작성함 (카운트 조회 최적화)
    - 검색 : 지역, 장르, 난이도, 활동성, 추천인원, 키워드로 검색 가능 (파라미터로 들어온 경우에만 검색 조건으로 추가됨)
    - 정렬 : 리뷰 높은 순, 이름순, 지역순 정렬 중 선택 가능

--- src/main/java/com/maemae/escaperoom/repository/ThemeRepositoryCustom.java
+++ src/main/java/com/maemae/escaperoom/repository/ThemeRepositoryCustom.java
@@ -2,6 +2,7 @@ package com.maemae.escaperoom.repository;
 
 import com.maemae.escaperoom.dto.ThemeDetailDTO;
 import com.maemae.escaperoom.dto.ThemeListDTO;
+import com.maemae.escaperoom.dto.ThemeSearchCondition;
 import com.maemae.escaperoom.dto.ThemeSimpleListDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,4 +14,5 @@ public interface ThemeRepositoryCustom {
     ThemeDetailDTO themeDetail(Long themeId);
     List<ThemeSimpleListDTO> themeListByCafeId(Long cafeId);
     List<ThemeSimpleListDTO> sameCafeOtherThemeList(Long themeId);
+    Page<ThemeListDTO> themeSearchPage(ThemeSearchCondition condition, Pageable pageable);
 }
